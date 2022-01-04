8fc3308a6a58ae0ce6f656321af878661b2bcf96
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-04 14:42:11 +0100

    unificato un solo treshold e tolleranza. Da questi due valori ricavo lunghezze temporali minime e massime per le sequenze di lunghezza "lasso" e scalate di "span"

--- estrazione_sequenze.m
+++ estrazione_sequenze.m
@@ -1,4 +1,5 @@
 function [seq_def] = estrazione_sequenze(data,variabili)
+global span lasso;
 %% Trovo le sequenze che sono più lunghe di 30 ore
 [dhour,d0]=mezzanotte(data(1,:));
 err=[];
@@ -6,11 +7,13 @@ d1=duration();
 for i=2:size(dhour,2)
     d1(i-1)= d0(i)-d0(i-1);
 end
-treshold='30:00:00';
+tolleranza=24/5;            % metto un 20% di tolleranza
+treshold=24 + tolleranza;       
+treshold=hours(treshold);
 err=find(d1(1,:)>treshold); 
 
 %% Ottengo sequenze di 24 ore
-righe=discrimina(variabili);
+righe=discrimina(variabili);  %da l'indice delle righe corrispondenti alle variabili che vogliamo usare in base al tipo di torre
 sequenze={};
 i=1;
 if ~ismember(i,err)
@@ -24,12 +27,10 @@ for i=2:size(dhour,2)-1
         sequenze{i,1}=[];
     end
 end
-panel_power= pp_dy (sequenze);
-%% creo sequenze di 6 giorni
-global span lasso;
-tolleranza=fix(24*lasso/10); %tolleranza sulla durata della sequenza del 10%
-treshold=(24*lasso)+tolleranza;
-treshold=hours(treshold);
+% panel_power= pp_dy (sequenze);
+%% creo sequenze di "lasso" giorni scalate di "span"
+
+treshold_lasso_max=treshold*lasso;
 seq_def={};
 i=1;
 j=size(sequenze,1);
@@ -41,11 +42,13 @@ while j-lasso+1>0   %estraggo sequenze da 6 giorni,interrompo quando i dati rima
 %             counter=[counter i];
 %         end
     end
-    seq_def{i,1}=seq;
+    if ~isempty(seq)
+        seq_def{i,1}=seq;
+        i=i+1;
+    end
     j=j-span;
-    i=i+1;
 end
-[~,~,d6,~]=durata_seq(seq_def,treshold,">=");   %trovo le sequenze più lunghe di 6 giorni
+[~,~,d6,~]=durata_seq(seq_def,treshold_lasso_max,">=");   %trovo le sequenze più lunghe di 6 giorni
 seq_def(d6,:)=[];                               %e le elimino
 [cell_idx, mat_idx] = picchi_anomali(seq_def);  % trovo le sequenze che presentano discontinuità sul min cellv che portano il valore istantaneamente sotto la soglia di 2000 mV
 for i=1:size(cell_idx,1)                          % trovi i campioni e gli assegno l'ultimo vaore valido campionato
@@ -54,8 +57,6 @@ for i=1:size(cell_idx,1)                          % trovi i campioni e gli asseg
     end
 end
 
-tolleranza=round(24/(lasso-1));                         %tolleranza sulla durata della sequenza di 20% di 24 ore
-treshold=24*(lasso-1)+tolleranza;
-treshold=hours(treshold);
+treshold_lasso_min=treshold*(lasso-1);
 [~,~,d6,~]=durata_seq(seq_def,treshold,"<=");   %trovo le sequenze più corte di 5 giorni
 seq_def(d6,:)=[];                               %e le elimino
