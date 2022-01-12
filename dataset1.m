4555946729ca17cdb5855b0f8ee102455e14bfaa
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-12 12:12:03 +0100

    aggiornamento modalità salvataggio

--- dataset1.m
+++ dataset1.m
@@ -37,8 +37,5 @@ parametri=strcat(string(lasso),'_',string(span),'_',string(int_predizione),'_',s
 path=strcat('risultati\',tralicci,{'\'},features,{'\'},parametri,{'\'});
 path=string(path);
 mkdir(path);
-dataset=strcat('dataset_',string(soglia_bad_mincellv),'_',string(soglia_good_mincellv),'_',string(soglia_bad_maxcellv),'_',string(soglia_good_maxcellv));
-save(strcat(path,dataset),"YTest","YTrain","XTest","XTrain","XVal","YVal","path","int_predizione", "lasso", "span","soglia_bad_mincellv", "soglia_good_mincellv", "soglia_bad_maxcellv", "soglia_good_maxcellv","quota_vs");
-
-
-
+dataset=strcat(string(soglia_bad_mincellv),'_',string(soglia_good_mincellv),'_',string(soglia_bad_maxcellv),'_',string(soglia_good_maxcellv),{'\'});
+save(strcat(path,dataset,'dataset'),"YTest","YTrain","XTest","XTrain","XVal","YVal","path","int_predizione", "lasso", "span","soglia_bad_mincellv", "soglia_good_mincellv", "soglia_bad_maxcellv", "soglia_good_maxcellv","quota_vs");
