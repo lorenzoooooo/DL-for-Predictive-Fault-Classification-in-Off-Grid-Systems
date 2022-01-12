5521808faf195e096f39c510579500b4a59f04a3
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-12 17:34:34 +0100

    per ogni istante corrispondente ad un picco anomalo di mincellv cancello tutte le variabili e le sostituisco con quelle relative all'istante precedente

--- picchi_anomali.m
+++ picchi_anomali.m
@@ -1,9 +1,10 @@
-function sequenze = picchi_anomali (sequenze)
+function sequenze = picchi_anomali (sequenze,variabili)
 for i=1:size(sequenze,1)
     for j=1:size(sequenze{i,1}.mincellvoltage,2)
         if sequenze{i,1}.mincellvoltage(j)<=2000
-            sequenze{i,1}.mincellvoltage(j)=sequenze{i,1}.mincellvoltage(j-1);  % trovo i campioni e gli assegno l'ultimo valore valido campionato
-            sequenze{i,1}.maxcellvoltage(j)=sequenze{i,1}.maxcellvoltage(j-1); 
+            for k=1:size(variabili.nb,1)
+                sequenze{i,1}.(variabili.nb(k))(j)=sequenze{i,1}.(variabili.nb(k))(j-1);    % trovo i campioni e gli assegno l'ultimo valore valido campionato
+            end
         end
     end
 end
