46e63e733791a1c28e08c3e426e8b1dc93207751
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-14 15:13:03 +0100

    levo comandi per vs set

--- dataset1.m
+++ dataset1.m
@@ -20,12 +20,12 @@ while ischar(a)                                         %ciclo che prende i dati
         XTest=[XTest; XTs{i,1}];
         YTest=[YTest; YTs(i,1)];
     end
-    for i=1:size(XVs,1)                                 %Mergio tutti i Tsset dlle singole torri in uno solo globale
-        XVal=[XVal; XVs{i,1}];
-        YVal=[YVal; YVs(i,1)];
-    end
-    tralicci=[tralicci; torre];
-    a = fgetl(fileID);
+%     for i=1:size(XVs,1)                                 %Mergio tutti i Tsset dlle singole torri in uno solo globale
+%         XVal=[XVal; XVs{i,1}];
+%         YVal=[YVal; YVs(i,1)];
+%     end
+%     tralicci=[tralicci; torre];
+%     a = fgetl(fileID);
 
 end
 fclose(fileID);
@@ -33,7 +33,7 @@ fclose(fileID);
 tralicci(1)=[];
 tralicci=nome_cartella(tralicci);
 features=nome_cartella(variabili.nome);
-parametri=strcat(string(lasso),'_',string(span),'_',string(int_predizione),'_',string(proporzione),'_',string(quota_vs));
+parametri=strcat(string(lasso),'_',string(span),'_',string(int_predizione),'_',string(proporzione));  %,'_',string(quota_vs)
 dataset=strcat(string(soglia_bad_mincellv),'_',string(soglia_good_mincellv),'_',string(soglia_bad_maxcellv),'_',string(soglia_good_maxcellv));
 path=strcat('risultati\',tralicci,{'\'},features,{'\'},parametri,{'\'},dataset,{'\'});
 path=string(path);
