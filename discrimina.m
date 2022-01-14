0aaa8675aa8be71d7703b6dc5923f47cf080780a
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-14 12:31:45 +0100

    solo per iotbox: inverto l'ordine dei for per far corrispondere nome e riga variabile

--- discrimina.m
+++ discrimina.m
@@ -13,8 +13,8 @@ if name =="var"
     end
 elseif name =="var_iotbox"
     load('var_iotbox.mat');
-    for i=1:size(var_iotbox,1)
-        for j=1:size(variabili.nome,1)
+    for j=1:size(variabili.nome,1)
+        for i=1:size(var_iotbox,1)
             if variabili.nome(j)==var_iotbox{i,1}
                 variabili.righe = [variabili.righe; i];
             end
