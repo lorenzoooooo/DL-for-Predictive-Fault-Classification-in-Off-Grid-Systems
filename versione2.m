14b81857e10dc98bbc73b9573b2a36bfc41fa595
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-17 16:45:07 +0100

    Seconda versione della bozza di dati. compatto tutte le colonne di campioni riguardanti lo stesso istante

---
+++ versione2.m
@@ -0,0 +1,46 @@
+function [dati, bug, bug_idx]  = versione2(temp_data,var)
+global torre tipo
+col=2;
+counter=1;
+dati=temp_data(:,1);
+y=zeros;
+bug_addr=strcat(tipo,{'\'},{torre},'\bug.txt');
+bug_addr=char(bug_addr);
+fileID = fopen(bug_addr,'w');
+fprintf(fileID,'coordinate delle variabili che hanno diversi campioni allo stesso istante di tempo\n');
+fprintf(fileID,'cerca in bug con le coordinate inserite in bug_idx per trovare le sequenze sospette:\n\tbug(bug_idx(1,1),bug_idx(1:2,2))\n\td=bug(1,bug_idx(1,2))\n\td1 = datetime(d,''ConvertFrom'',''excel'')\n');
+bug_idx={};
+while col<size(temp_data,2)                                 %scelgo la colonna
+    if temp_data(1,col)==temp_data(1,col+1)
+        counter=find(temp_data(1,col)==temp_data(1,:));     %conto quali colonne rappresentano lo stesso istante
+        sottomatrice=temp_data(:,counter);                  %estraggo la sottomatrice di dati rappresentanti lo stesso istante
+        valori_NaN = isnan(sottomatrice);
+        stringa_NaN=~zeros(1,size(sottomatrice,2));
+        for j=1:size(var,1)
+            k=j+1;
+            if valori_NaN(k,:)== stringa_NaN                %se una variabile non è stata sovrascritta passo alla riga seguente
+                continue;
+            end
+            a=find(valori_NaN(k,:)==0);
+            if size(a,2)>1
+                for h=1:size(a,2)                           %se size(a)>1 allora ci sono diversi valori allo stesso istante
+                    fprintf(fileID,'(%d,%d)\n',k,col+a(1,h)-1);
+                    y(1,h)=sottomatrice(k,a(1,h));
+                    bug_idx=[bug_idx;[k,col+a(1,h)-1]];
+                end
+                fprintf(fileID,'\n');
+                sottomatrice(k,1)= mean(y,2);               %il dato acquisito è la media dei campioni
+            else
+                sottomatrice(k,1)=sottomatrice(k,a);
+            end
+        end
+        col=col+length(counter);
+    else
+        sottomatrice=temp_data(:,col);
+        col=col+1;
+    end
+    dati=[dati sottomatrice(:,1)];
+end
+fclose(fileID);
+bug=temp_data;
+bug_idx=cell2mat(bug_idx);
