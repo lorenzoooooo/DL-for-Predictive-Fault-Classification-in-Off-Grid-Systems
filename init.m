efd05fa3a64122bd7ee1961d5e08b8ce31724eea
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-17 14:29:02 +0100

    clear qualche varabile durante l'esecuzione

--- init.m
+++ init.m
@@ -22,11 +22,11 @@ time = transpose(time);
 count = transpose(sqldata{:,2});
 codice = transpose(sqldata{:,3});
 bozza_dati = {time;count;codice};
-
+clearvars time count codice;
+%% prima versione della bozza di dati
 temp_data=versione1(bozza_dati,var);
 
-% data=temp_data;
-%% compatto tutte le colonne di campioni riguardanti lo stesso istante
+%% Seconda versione della bozza di dati. compatto tutte le colonne di campioni riguardanti lo stesso istante
 col=2;
 counter=1;
 bozza_dati=temp_data(:,1);
@@ -37,26 +37,26 @@ fileID = fopen(bug_addr,'w');
 fprintf(fileID,'coordinate delle variabili che hanno diversi campioni allo stesso istante di tempo\n');
 fprintf(fileID,'cerca in bug con le coordinate inserite in bug_idx per trovare le sequenze sospette:\n\tbug(bug_idx(1,1),bug_idx(1:2,2))\n\td=bug(1,bug_idx(1,2))\n\td1 = datetime(d,''ConvertFrom'',''excel'')\n');
 bug_idx={};
-while col<size(temp_data,2) %scelgo la colonna
+while col<size(temp_data,2)                                 %scelgo la colonna
     if temp_data(1,col)==temp_data(1,col+1)
-        counter=find(temp_data(1,col)==temp_data(1,:)); %conto quante colonne rappresentano lo stesso istante
-        sottomatrice=temp_data(:,counter); %estraggo la sottomatrice di dati rappresentanti lo stesso istante
+        counter=find(temp_data(1,col)==temp_data(1,:));     %conto quali colonne rappresentano lo stesso istante
+        sottomatrice=temp_data(:,counter);                  %estraggo la sottomatrice di dati rappresentanti lo stesso istante
         valori_NaN = isnan(sottomatrice);
         stringa_NaN=~zeros(1,size(sottomatrice,2));
-        for j=1:righe_var
+        for j=1:size(var,1)
             k=j+1;
-            if valori_NaN(k,:)== stringa_NaN %se una variabile non è stata sovrascritta passo alla riga seguente
+            if valori_NaN(k,:)== stringa_NaN                %se una variabile non è stata sovrascritta passo alla riga seguente
                 continue;
             else
                 a=find(valori_NaN(k,:)==0);
                 if size(a,2)>1
-                    for h=1:size(a,2) %se size(a)>1 allora ci sono diversi valori allo stesso istante
+                    for h=1:size(a,2)                       %se size(a)>1 allora ci sono diversi valori allo stesso istante
                         fprintf(fileID,'(%d,%d)\n',k,col+a(1,h)-1);
                         y(1,h)=sottomatrice(k,a(1,h));
                         bug_idx=[bug_idx;[k,col+a(1,h)-1]];
                     end
                     fprintf(fileID,'\n');
-                    sottomatrice(k,1)= mean(y,2); %il dato acquisito è la media dei campioni
+                    sottomatrice(k,1)= mean(y,2);           %il dato acquisito è la media dei campioni
                 else
                     sottomatrice(k,1)=sottomatrice(k,a);
                 end
