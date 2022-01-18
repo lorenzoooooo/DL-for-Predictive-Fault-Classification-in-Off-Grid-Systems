e50fe8ddcd834ce9e3b5c50c9be8e4d8dbf05543
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-18 18:09:06 +0100

    sto creando un init più semplice e modulare. Andro a fare le operazioni su una variabile alla volta

--- init.m
+++ init.m
@@ -17,97 +17,22 @@
 %     end
 % end
 % time = datetime(sqldata{:,1});
-% time = convertTo(time,'excel');
+% % time = convertTo(time,'excel');
 % time = transpose(time);
 % count = transpose(sqldata{:,2});
 % codice = transpose(sqldata{:,3});
-% bozza_dati = {time;count;codice};
-% clearvars time count codice;
-% %% prima versione della bozza di dati
-% 
-% temp_data=versione1(bozza_dati,var);
-% 
-% %% Seconda versione della bozza di dati. compatto tutte le colonne di campioni riguardanti lo stesso istante
-% 
-% [temp_data,bug,bug_idx]=versione2(temp_data,var);
+% diag= transpose(sqldata{:,4});
+% bozza_dati = {time;count;codice;diag};
+% clearvars time count codice diag;
 
-%% Creo una colonna iniziale con i valori per tutte le variabili
-
-% temp_data=versione3(temp_data,var);
-
-%% sostituisco NaN con il valore precedente diverso da zero più vicino
-sottomatrice=temp_data(:,1);
-data=[];
-if name == "var"
-    energy_idx=find(var{:,3}==2);
-    energy_idx=energy_idx+1;
-    meteo_idx=find(var{:,3}==1);
-    meteo_idx=meteo_idx+1;
+%%
+coord=cell(size(var,1),1);
+for i=1:size(var,1)
+    mystruct.name=var{i,1};
+    idx= find(eq(bozza_dati{3,:}, var{i,2}));
+    mystruct.value=bozza_dati{2,1}(idx);
+    mystruct.time=bozza_dati{1,1}(idx);
+    mystruct.diag=bozza_dati{4,1}(idx);
+    coord{i,1}=mystruct;
 end
-for i=2:size(temp_data,2)
-    data=[data sottomatrice];
-    c=~isnan(temp_data(:,i));
-    sottomatrice(1,1)=temp_data(1,i);
-%     if c                                            %se tt i val sulla colonna sono diversi da NaN
-%         sottomatrice=temp_data(:,i);
-%         continue
-%     end
-    if ~c(meteo_idx)                                      %se tutte le variabili della centralina mete sono NaN allora, se i valori delle altre variabili sono NaN vuol dire non sono variati rispetto all'ultimo campione e quindi riscrivo l'ultimo campione
-        sottomatrice(meteo_idx,1)=NaN;
-        for j=3:size(temp_data,1)
-             if c(j)                                %se il valore non è NaN passo alla prossima riga
-                 sottomatrice(j,1)=temp_data(j,i);
-                 continue
-             else                                   %altrimenti ricopio l'ultimo campione diverso da NaN
-                 counter=isnan(temp_data(j,i-1:-1:1));
-                 counter=find(counter==0,1);
-                 counter=i-counter;                 %colonna della variabile con valore numerico
-                 sottomatrice(j,1)=temp_data(j,counter);
-             end
-        end
-    else                                            %se l'irradiazione è diverso da NaN allora ricopio la matrice
-        if c(3:end)==zeros(size(c(3:end))) & i~=size(temp_data,2)
-            sottomatrice(:,1)=temp_data(:,i);
-        else
-            sottomatrice(2,1)=temp_data(2,i);
-            for j=3:size(temp_data,1)
-                if c(j)                             %se il valore non è NaN passo alla prossima riga
-                    sottomatrice(j,1)=temp_data(j,i);
-                    continue
-                else                                %altrimenti ricopio l'ultimo campione diverso da NaN
-                    counter=isnan(temp_data(j,i-1:-1:1));
-                    counter=find(counter==0,1);
-                    counter=i-counter;              %colonna della variabile con valore numerico
-                    sottomatrice(j,1)=temp_data(j,counter);
-                end
-             end
-        end
-    end
-end
-data=[data sottomatrice];
 
-%% interpolo i valori NaN che corrispondono all'asincronità dei pacchetti
-
-x=[1:size(data,2)];
-c=[];
-y=[];
-p=[];
-p(1,:)=data(1,:);
-c=~isnan(data(2,:));
-c1=find(c);                             %valori campionati di irradiazione
-c0=find(c==0);                          %valori NaN di irradiazione
-y=data(2,:);
-p(2,:)=interp1(x(c1),y(c1),x,'linear'); %interpolo valori irradiazione
-for k=3:size(data,1)                    %interpolo valori var energy management
-    y=data(k,:);
-    c_k=~isnan(data(k,:));
-    c1_k=find(c_k);
-    p(k,:)=interp1(x(c1_k),y(c1_k),x,'linear');
-end
-data=p;
-
-%% aggiungo correzione anomalie
-anomalie=find(data(3,:)<=2500);
-for i=1:size(anomalie,2)
-    data(2:end,anomalie(i))=data(2:end,anomalie(i)-1);
-end
