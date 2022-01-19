6bc31ecd222d0b32df96f2347d05f1b6a340ca2f
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-19 17:56:28 +0100

    provo ad inserirre correttamente un campione prima di quello precedente quando l'intervallo tra 2 dati è maggiore di 20 min

--- init.m
+++ init.m
@@ -16,16 +16,18 @@
 %         mkdir(string(strcat(tipo,{'\'},{torre})));
 %     end
 % end
-% time = datetime(sqldata{:,1});
-% % time = convertTo(time,'excel');
-% time = transpose(time);
-% count = transpose(sqldata{:,2});
-% codice = transpose(sqldata{:,3});
-% diag= transpose(sqldata{:,4});
-% bozza_dati = {time;count;codice;diag};
-% clearvars time count codice diag;
-
+time = datetime(sqldata{:,1});
+% time = convertTo(time,'excel');
+time = transpose(time);
+count = transpose(sqldata{:,2});
+codice = transpose(sqldata{:,3});
+diag= transpose(sqldata{:,4});
+bozza_dati = {time;count;codice;diag};
+clearvars time count codice diag;
 %%
+std_freq=900;
+max_timeout=seconds(1200);
+
 coord=cell(size(var,1),1);
 for i=1:size(var,1)
     mystruct.name=var{i,1};
@@ -36,3 +38,40 @@ for i=1:size(var,1)
     coord{i,1}=mystruct;
 end
 
+for j=10
+    int=diff(coord{j,1}.time);                         % differenza tra 2 campioni consecutivi: X(2)-X(1)
+    % int_q=floor(int,std_freq);
+    int_idx=find(int>max_timeout)+1;                    % voglio l'indice del campione X(2)
+    no_diag=[];
+    for i=1:size(int_idx,2)
+        if coord{j,1}.diag(int_idx(i))==0              % trovo di int_idx quei campioni che hanno diag=0
+            no_diag=[no_diag int_idx(i)];
+        end
+    end
+    p{j,1}=coord{j,1};
+    d{j,1}=no_diag;
+    for i=1:size(no_diag,2)-1
+        coord{j,1}.time=[coord{j,1}.time(1:no_diag(i)-1) coord{j,1}.time(no_diag(i))-seconds(std_freq) coord{j,1}.time(no_diag(i):end)];
+        coord{j,1}.value=[coord{j,1}.value(1:no_diag(i)-1) coord{j,1}.value(no_diag(i)-1) coord{j,1}.value(no_diag(i):end)];
+        coord{j,1}.diag=[coord{j,1}.diag(1:no_diag(i)-1) coord{j,1}.diag(no_diag(i)-1) coord{j,1}.diag(no_diag(i):end)];
+        no_diag(i+1)=no_diag(i+1)+1;
+    end
+    coord{j,1}.time=[coord{j,1}.time(1:no_diag(i)-1) coord{j,1}.time(no_diag(i))-seconds(std_freq) coord{j,1}.time(no_diag(i):end)];
+    coord{j,1}.value=[coord{j,1}.value(1:no_diag(i)-1) coord{j,1}.value(no_diag(i)-1) coord{j,1}.value(no_diag(i):end)];
+    coord{j,1}.diag=[coord{j,1}.diag(1:no_diag(i)-1) coord{j,1}.diag(no_diag(i)-1) coord{j,1}.diag(no_diag(i):end)];
+    unchanged{j,1}=no_diag;
+end
+
+
+j=10;
+%     b=48;
+%     a=find(p.time==coord{10,1}.time(b));
+    figure;
+    plot(coord{j,1}.time, coord{j,1}.value,'b');
+    hold on;
+    plot(p{j,1}.time,p{j,1}.value);
+    hold off;
+
+% addr=strcat(tipo,{'\'},{torre},{'\'},{torre});
+% addr=char(addr);
+% save(addr);
