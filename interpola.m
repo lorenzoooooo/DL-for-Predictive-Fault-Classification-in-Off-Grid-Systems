78d22c7553a5ebb936f90845f7fc3a1e7cd20e1a
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-01-28 17:59:16 +0100

    gli ultimi valori se sono 0 lui li mette a NaN e quindi vanno forzati a 0

--- interpola.m
+++ interpola.m
@@ -8,3 +8,8 @@ c0=find(coord.diag==0);
 % c1=find(coord.diag==1);
 coord.value=interp1(x(c0),y(c0),x);
 coord.time=x;
+
+% gli ultimi valori se sono 0 lui li mette a NaN e quindi vanno forzati a 0
+n=isnan(coord.value);
+n=find(n==1);
+coord.value(n)=y(n);
