077a22ebcb891a69e2ee3ccae65ea1d9107ceb1b
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-02-01 18:07:40 +0100

    faccio la media e la dev_std soltanto nell'intrvallo temporale tra la prima sequenza e l'ultima

--- normalizzazione.m
+++ normalizzazione.m
@@ -1,9 +1,11 @@
-function sequenze = normalizzazione(data,sequenze,variabili)
+function sequenze = normalizzazione(nuova_struct,sequenze,variabili)
 media=[];
 dev_std=[]; 
-for j=1:size(variabili.righe,1)
-    media(j,1)=mean(data(variabili.righe(j),:));
-    dev_std(j,1)=std(data(variabili.righe(j),:));
+start=find(convertTo(nuova_struct.time,'excel')==sequenze{1,1}.time(1));
+finish=find(convertTo(nuova_struct.time,'excel')==sequenze{end,1}.time(end));
+for j=1:size(variabili.nb,1)
+    media(j,1)=mean(nuova_struct.(variabili.nb(j))(start:finish));
+    dev_std(j,1)=std(nuova_struct.(variabili.nb(j))(start:finish));
 end
 for i=1:size(sequenze,1)
     for j=1:size(sequenze{i,1}.time,2)
