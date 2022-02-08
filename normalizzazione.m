0642d0738d881c9190bcbb25ecc89a00c11edf78
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-02-08 11:19:43 +0100

    otimizzazione levando un nest di for superfluo

--- normalizzazione.m
+++ normalizzazione.m
@@ -8,9 +8,7 @@ for j=1:size(variabili.nb,1)
     dev_std(j,1)=std(nuova_struct.(variabili.nb(j))(start:finish));
 end
 for i=1:size(sequenze,1)
-    for j=1:size(sequenze{i,1}.time,2)
-        for k=1:size(variabili.nb,1)
-            sequenze{i,1}.(variabili.nb(k))(j)=(sequenze{i,1}.(variabili.nb(k))(j)-media(k,1))/dev_std(k,1);
-        end
+    for k=1:size(variabili.nb,1)
+        sequenze{i,1}.(variabili.nb(k))=(sequenze{i,1}.(variabili.nb(k))-media(k,1))/dev_std(k,1);
     end
 end
