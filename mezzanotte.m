ff87033ffbe5ac1414ad331be7fcfca4e9be185a
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-03-21 11:54:26 +0100

    update variabili

--- mezzanotte.m
+++ mezzanotte.m
@@ -1,4 +1,3 @@
-
 function [dhour, d0] = mezzanotte(tempo)
     d=tempo;
     % Trovo i campioni che scandiscono la fine della giornata
@@ -20,61 +19,3 @@ function [dhour, d0] = mezzanotte(tempo)
         end
     end
     d0=tempo(dhour);
-
-    %% una volta che si è spenta la torre, gli unici istanti di funzionamento sono durante il giorno e quindi
-    % per prelevarli devo concatenare a dhour campioni presi tra le 6 e e 14
-%     t1=dhour(end);
-%     a=[];
-%     b=[];
-%     size_month=[];
-%     i=t1:size(tempo,2);
-%     a=find(hour(d(i))==6 | hour(d(i))==7 |hour(d(i))==8|hour(d(i))==9 |hour(d(i))==10|hour(d(i))==11|hour(d(i))==12|hour(d(i))==13|hour(d(i))==14);
-%     a=a+t1-1;
-%     dday=day(datetime(tempo(1,a),'ConvertFrom','excel'));
-%     dmonth=month(datetime(tempo(1,a),'ConvertFrom','excel'));
-% 
-%     %% Trovo l'indice in cui avviene il cambio di mese e il numero di giorni nel mese
-%     counter=1;
-%     size_month(counter,1)= 1;
-%     size_month(counter,2)="NaN";
-%     counter=counter+1;
-%     for i=2:size(dmonth,2)          
-%         if dmonth(i)~=dmonth(i-1)
-%             size_month(counter,1)= i; 
-%             size_month(counter,2)=lung_mesi(dmonth(i-1));
-%             counter=counter+1;
-%         end
-%     end
-%     size_month(counter,1)=size(dmonth,2);
-%     size_month(counter,2)=lung_mesi(dmonth(end));
-%     
-%     %% trovo l'indice dell'ultima istanza di ogni giorno per ogni mese
-%     c=[];
-%     for i=1:size(size_month,1)-1                        %per ogni intervallo che rappresenta un mese
-%         if dday(size_month(i,1))==1                     %se il mese è campionato dal primo
-%             for j=1:size_month(i+1,2)                   %per ogni giorno del mese
-%                 counter=find(dday(size_month(i,1):size_month(i+1,1))==j,1,'last');
-%                 if isempty(counter)
-%                     b(i,j)=0;
-%                 else
-%                     b(i,j)=counter;
-%                     b(i,j)=b(i,j)+size_month(i,1);
-%                     c=[c b(i,j)];
-%                 end
-%             end
-%         else                                            %se il mese NON è campionato dal primo
-%             for j=dday(i):size_month(i+1,2)             %per ogni giorno del mese
-%                 counter=find(dday(size_month(i,1):size_month(i+1,1))==j,1,'last');
-%                 if isempty(counter)
-%                     b(i,j)=0;
-%                 else
-%                     b(i,j)=counter;
-%                     b(i,j)=b(i,j)+size_month(i,1);
-%                     c=[c b(i,j)];
-%                 end
-%             end
-%         end
-%     end
-%     c(1,end)=size_month(end,1);
-%     a=a(c);
-%     dhour=[dhour a];
