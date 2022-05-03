dc890edee25e6e580299235f4330d9b9d96e9b04
lorenzoooooo <giraldi.1649909@studenti.uniroma1.it>
2022-05-03 11:33:03 +0200

    versione con 3 fully connected (2 Relu e 1 Softmax)

--- kfold.m
+++ kfold.m
@@ -2,19 +2,24 @@ close all force;
 clear;
 
 input('controlla che stai usando il giusto dataset!');
-dataset_path=['risultati_int\t13008_t16399_t1059_t1021_t1025\mincellvoltage_panelpower\1_1_1_1_0.25\dataset'];
+% dataset_path=['risultati_int\t13008_t16399_t1059_t1021_t1025\mincellvoltage_panelpower\3_1_1_3_0.25\dataset'];
+dataset_path=['risultati_int\t13008_t16399_t1059_t1021\mincellvoltage_panelpower_soc_irradiation_totbatterycurrent\1_1_7_3_0.25\3200\dataset'];
 load(dataset_path, 'X', 'Y','path');
 
-inputSize = 2;
+inputSize = 5;
 numHiddenUnits =15;
 numClasses = 2;
 maxEpochs = 8;
-miniBatchSize =9;
+miniBatchSize =10;
 lr=0.04;
 
 layers = [ ...
     sequenceInputLayer(inputSize)
     bilstmLayer(numHiddenUnits,'OutputMode','last')
+    fullyConnectedLayer(19)
+    reluLayer
+    fullyConnectedLayer(10)
+    reluLayer
     fullyConnectedLayer(numClasses)
     softmaxLayer
     classificationLayer];
@@ -28,15 +33,18 @@ options = trainingOptions('adam', ...
     'GradientThreshold',1, ...
     'MaxEpochs',maxEpochs, ...
     'MiniBatchSize',miniBatchSize, ...
-    'SequenceLength','longest', ...
     'Shuffle','every-epoch', ...
     'Verbose',0);
 %     'Plots','training-progress');
 
+
+n=randperm(numel(Y));
+X=X(n);
+Y=Y(n);
+prova=[];
+
 n_runs=10;
 k_fold=4;
-testX={};
-a=[];
  for z=1:n_runs
         fprintf('--- RUN %i / %i ---\n', z, n_runs);
         rng(z,'Threefry');
@@ -49,25 +57,10 @@ a=[];
             trainY = Y(cv.training(i));
             testX(:,1)= X(cv.test(i),:);
             trainX(:,1) = X(cv.training(i),:);
-% Obtain Sequence length
-            numObservations = numel(trainX);
-            for j=1:numObservations
-                sequence = trainX{j};
-                sequenceLengths(j) = size(sequence,2);
-            end
-            numObservationsTest = numel(testX);
-            for j=1:numObservationsTest
-                sequence = testX{j};
-                sequenceLengthsTest(j) = size(sequence,2);
-            end
 % Training
             net = trainNetwork(trainX,trainY,layers,options);
 % Test
-            YPred(cv.test(i),z) = classify(net,testX, ...
-            'SequenceLength','longest');
-            
-            a(i,1)=numel(find(trainY=='0'));
-            a(i,2)=numel(find(testY=='0'));
+            YPred(cv.test(i),z) = classify(net,testX);
         end
 % accuracy    
         [cm(:,:,z),order]=confusionmat(Y,YPred(:,z),'Order',categories(Y));     
@@ -79,27 +72,34 @@ a=[];
         acc(z,1) = sum(YPred(:,z) == Y)./numel(Y);
         fprintf('\t--- Final accuracy on run %i: %.2f%% ---\n\n', z, 100*acc(z,1));
         drawnow;
-    end
-    
+ end
+ 
+% figure;    
+% cm_avg=confusionchart(round(mean(cm,3)),order);
+% cm_avg.Title=sprintf('Mean confusion matrix');
+% cm_avg.RowSummary = 'row-normalized';
+% cm_avg.ColumnSummary = 'column-normalized';
+
 cm_avg=mean(cm,3);
-fprintf('Average confusion matrix (true classes by rows):\n');
+fprintf('\n  Average confusion matrix (true classes by rows):\n');
 disp(cm_avg);  
 
 cm_std=std(cm,0,3);
-fprintf('Std. confusion matrix (true classes by rows):\n');
+fprintf('  Std. confusion matrix (true classes by rows):\n');
 disp(cm_std);  
 
 acc_avg=mean(acc*100);
 acc_std=std(acc*100,0);
-fprintf('Overall classification accuracy: %.2f%% %c %.2f%%\n', acc_avg,char(177),acc_std);
+fprintf('  Overall classification accuracy: %.2f%% %c %.2f%%\n', acc_avg,char(177),acc_std);
 
 err_avg=mean(100*(1-acc));
 err_std=std(100*(1-acc),0);
-fprintf('\nOverall classification error: %.2f%% %c %.2f%%\n\n', err_avg,char(177),err_std);
+fprintf('\n  Overall classification error: %.2f%% %c %.2f%%\n\n', err_avg,char(177),err_std);
 
 %% salvataggio
-
-file=strcat(strcat(string(day(datetime)),'-',string(month(datetime)),'_',string(numHiddenUnits),'_',string(maxEpochs),'_',string(lr),'_',string(options.LearnRateDropFactor),'_',string(round(acc_avg/100,2)),'_kfold','_',string(n_runs),'_',string(k_fold),'_',string(options.Shuffle),'_',string(miniBatchSize)));
+file=strcat(strcat(string(day(datetime)),'-',string(month(datetime)),'_',string(numHiddenUnits),'_',string(maxEpochs),'_',string(lr),'_',string(options.LearnRateDropFactor),'_',string(round(acc_avg/100,2)),'_kfold','_',string(n_runs),'_',string(k_fold),'_',string(miniBatchSize)));
+% path="risultati_3fc\t13008_t16399_t1059_t1021_t1025\mincellvoltage_panelpower\3_1_1_3_0.25\";
+path="risultati_3fc\t13008_t16399_t1059_t1021\mincellvoltage_panelpower_soc_irradiation_totbatterycurrent\1_1_7_3_0.25\";
 path_def=strcat(path,{'\'},file,{'\'});
 mkdir(path_def);
 save(strcat(path_def,'risultati'));
